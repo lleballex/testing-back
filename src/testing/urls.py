62b14e1bf75555b17586853d2405260ce55f7a30
lleballex <lleballex@yandex.ru>
2020-09-24 16:45:16 +0300

    Add '/api/' to all urls

--- src/testing/urls.py
+++ src/testing/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('account/', include('account.urls')),
-    path('tests/', include('tests.urls')),
+    path('api/account/', include('account.urls')),
+    path('api/tests/', include('tests.urls')),
 ]
